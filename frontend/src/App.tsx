bd530f6632f4e2b60e9f77953cb1ea9813193df5
tim <t.williams.im@gmail.com>
2021-03-19 22:23:11 -0400

    add bc map to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+//import logo from './logo.svg';
 import './App.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet'
+
+// function LocationMarker() {
+//   const [position, setPosition] = useState(null)
+//   const map = useMapEvents({
+//     click(e) {
+//       //map.locate()
+//       setPosition(e.latlng)
+//     },
+//     locationfound(e) {
+//       setPosition(e.latlng)
+//       map.flyTo(e.latlng, map.getZoom())
+//     },
+//   })
+
+//   return position === null ? null : (
+//     <Marker position={position}>
+//       <Popup>You are here</Popup>
+//     </Marker>
+//   )
+// }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <MapContainer style={{ height: '100vh', width: '100wh' }} center={[53.726669, -127.647621]} zoom={6} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[53.726669, -127.647621]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
