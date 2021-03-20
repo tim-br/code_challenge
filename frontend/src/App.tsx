d181c9c9ae163804e57ee0cab02bda13fde9748e
tim <t.williams.im@gmail.com>
2021-03-20 09:31:03 -0400

    refactor to use class component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import './App.css';
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet'
 import { LatLngExpression } from "leaflet";
+import { ReactComponent } from '*.svg';
 
 // function LocationMarker() {
 //   const [position, setPosition] = useState(null)
@@ -51,37 +52,40 @@ const AddMarker = () => {
   }
 };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <MapContainer style={{ height: '100vh', width: '100wh' }} center={[53.726669, -127.647621]} zoom={6} scrollWheelZoom={true} touchZoom={true}>
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          {/* <Marker position={[53.726669, -127.647621]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker> */}
-          <AddMarker/>
-        </MapContainer>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <MapContainer style={{ height: '100vh', width: '100wh' }} center={[53.726669, -127.647621]} zoom={6} scrollWheelZoom={true} touchZoom={true}>
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {/* <Marker position={[53.726669, -127.647621]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker> */}
+            <AddMarker/>
+          </MapContainer>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          {/* <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
