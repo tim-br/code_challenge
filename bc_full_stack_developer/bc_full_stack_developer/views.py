94f4a1d7018c45119334b32db769e9df1b2cd7e3
tim <t.williams.im@gmail.com>
2021-03-20 13:01:08 -0400

    insert apirequest into db; mount get_count to container

--- bc_full_stack_developer/bc_full_stack_developer/views.py
+++ bc_full_stack_developer/bc_full_stack_developer/views.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import decimal
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import requests
@@ -19,10 +20,13 @@ def my_view(request):
     url = Template("https://openmaps.gov.bc.ca/geo/pub/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=pub%3AWHSE_ADMIN_BOUNDARIES.BCHA_CMNTY_HEALTH_SERV_AREA_SP&srsname=EPSG%3A4326&cql_filter=INTERSECTS(SHAPE%2CSRID%3D4326%3BPOINT($lon+$lat))&propertyName=CMNTY_HLTH_SERV_AREA_CODE%2CCMNTY_HLTH_SERV_AREA_NAME&outputFormat=application%2Fjson")
     longitude = request.query_params.get('longitude')
     latitude= request.query_params.get('latitude')
-    # ip_address = get_client_ip(request)
-    # ar = ApiRequest.new()
-    # ar.ip_address = ip_address
-    # ar.lat = 
+    ip_address = get_client_ip(request)
+    ar = ApiRequest()
+    ar.ip_address = ip_address
+    ar.lat = decimal.Decimal(latitude)
+    ar.lon = decimal.Decimal(longitude)
+    ar.date = datetime.now()
+    ar.save()
     final_url = url.substitute({'lon': longitude, 'lat': latitude})
     response = requests.get(final_url)
     return Response(data=response.json())
