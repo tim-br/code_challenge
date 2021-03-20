6e0a94ddaaa6692064a78be77a9fa5a8395743ce
tim <t.williams.im@gmail.com>
2021-03-20 10:12:29 -0400

    display community health service area after clicking on map

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet'
 import { LatLngExpression } from "leaflet";
 import { ReactComponent } from '*.svg';
+import { parseJsonConfigFileContent } from 'typescript';
 
 // function LocationMarker() {
 //   const [position, setPosition] = useState(null)
@@ -39,12 +40,15 @@ const AddMarker = (props: Props) => {
       let longitude = latLngExp.lng
       setPosition(latLngExp)
       const urlTemplate = `http://localhost:8000/myview/?longitude=${longitude}&latitude=${latitude}`
-      console.log("urltemplate")
-      console.log(urlTemplate)
-      props.handler()
       fetch(urlTemplate)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => {
+          if(data.totalFeatures === 0){
+            props.handler("INVALID LOCATION")
+          } else {
+            props.handler(data.features[0].properties.CMNTY_HLTH_SERV_AREA_NAME)
+          }
+        })
 
       /* CODE TO ADD NEW PLACE TO STORE (check the source code) */
     },
@@ -60,7 +64,7 @@ const AddMarker = (props: Props) => {
 class App extends React.Component {
 
   state = { 
-    someVar: "hello"
+    someVar: "Click a location on the map to view your Community Health Service Area"
   }
 
   constructor(props: {} | Readonly<{}>) {
@@ -69,9 +73,9 @@ class App extends React.Component {
     this.handler = this.handler.bind(this)
   }
 
-  handler() {
+  handler(newValue: string) {
     this.setState({
-      someVar: 'some value'
+      someVar: newValue
     })
   }
 
@@ -80,7 +84,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <MapContainer style={{ height: '100vh', width: '100wh' }} center={[53.726669, -127.647621]} zoom={6} scrollWheelZoom={true} touchZoom={true}>
+          <MapContainer style={{ height: '100vh', width: '100wh' }} center={[50.726669, -120.647621]} zoom={6} scrollWheelZoom={true} touchZoom={true}>
             <TileLayer
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -93,16 +97,8 @@ class App extends React.Component {
             <AddMarker handler = {this.handler} />
           </MapContainer>
           <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+            Community Health Service Area
           </p>
-          {/* <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
           <div>{this.state.someVar}</div>
         </header>
       </div>
