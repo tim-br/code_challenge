64640b1bbae107f7fc71d5235d9bb3011cfb5479
tim <t.williams.im@gmail.com>
2021-03-19 21:58:54 -0400

    add leaflet types dep

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function App() {
   return (
